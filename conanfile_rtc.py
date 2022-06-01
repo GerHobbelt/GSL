aa8c47671d7a036411ea43300203e84fa2adee20
Chris Warner <cwarner@esri.com>
2022-06-01 15:51:47 -0700

    build: add conanfile

---
+++ conanfile_rtc.py
@@ -0,0 +1,21 @@
+from conans import ConanFile
+
+
+class GSLConan(ConanFile):
+    name = "gsl"
+    version = "3.1.0"
+    url = "https://github.com/Esri/gsl"
+    license = "https://github.com/Esri/gsl/blob/master/LICENSE"
+    description = "Guidelines Support Library"
+
+    # RTC specific triple
+    settings = "platform_architecture_target"
+
+    def package(self):
+        base = self.source_folder + "/"
+        relative = "3rdparty/gsl/"
+
+        # headers
+        self.copy("*.h", src=base + "include/gsl", dst=relative + "include/gsl")
+
+        # no libraries; header-only
