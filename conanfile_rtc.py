8705d3fffbc7a48bfc22d57b371b125d6604e04c
Sivabalan Muthukrishnan <smuthukrishnan@esri.com>
2023-10-10 17:30:56 -0700

    build: Update gsl version to 4.0.0 in conan file

--- conanfile_rtc.py
+++ conanfile_rtc.py
@@ -3,7 +3,7 @@ from conans import ConanFile
 
 class GSLConan(ConanFile):
     name = "gsl"
-    version = "3.1.0"
+    version = "4.0.0"
     url = "https://github.com/Esri/gsl"
     license = "https://github.com/Esri/gsl/blob/master/LICENSE"
     description = "Guidelines Support Library"
